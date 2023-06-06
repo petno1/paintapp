487ad01c06b840bd2c73c0c87eb8a2ba44f82a56
petno1 <56641096+petno1@users.noreply.github.com>
2023-06-06 09:21:41 +0200

    Add files via upload

---
+++ himmelapp.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import cv2
+st.title('ISO paint-thingy')
+
+path = r"C:\Users\petno\Downloads\SampleInput.jpg"
+
+
+@st.cache_data
+def load_img(path, size_scale):
+    img = cv2.imread(path)
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img, gray
+
+hour_to_filter = st.slider('Resize Percent', 1, 100, 100)
+img, gray = load_img(path, hour_to_filter)
+width = int(img.shape[1] * hour_to_filter / 100)
+height = int(img.shape[0] * hour_to_filter / 100)
+dim = (width, height)
+# resize image
+img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.image(img)
+
+!pip freeze
